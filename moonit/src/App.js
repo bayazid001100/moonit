7656dfc3f329cc88695dc11a920f04dd72083f47
bayazid001100 <34455165+bayazid001100@users.noreply.github.com>
2021-01-21 16:02:21 +0600

    Footer

--- moonit/src/App.js
+++ moonit/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Nav from './Nav';
+import FinalFooter from './FinalFooter';
 
 function App() {
   return (
     <div>
       <Nav/>
+      <FinalFooter />
     </div>
   );
 }
