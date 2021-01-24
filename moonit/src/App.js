c7cdc7aeec417fe53509517454019cfe94eb0e5d
bayazid001100 <34455165+bayazid001100@users.noreply.github.com>
2021-01-24 17:42:40 +0600

    Home

--- moonit/src/App.js
+++ moonit/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import Nav from './Nav';
 import FinalFooter from './FinalFooter';
+import Slider from './components/slider/slider';
+import Banner from './components/banner/Banner';
 
 function App() {
   return (
     <div>
       <Nav/>
+      <Banner/>
+      <Slider/>
       <FinalFooter />
     </div>
   );
